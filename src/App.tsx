d167eaeae07b2c0a36ea29575f1fcbd3a0628d34
DmytroChystov <ramsneg2@gmail.com>
2025-11-30 11:53:25 +0100

    hmw_06

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // Lessons
 // Homeworks
-import Lesson_06 from "./lessons/lesson_06/Lesson_06";
+// import Lesson_06 from "./lessons/lesson_06/Lesson_06";
+import Homerwork_06 from "./homerworks/homerwork_06/Homerwork_06";
 function App() {
   return (
   <>
-  <Lesson_06 />
+  {/* <Lesson_06 /> */}
+  <Homerwork_06 />
   </>
   )
 }
---
+++ src/homerworks/homerwork_06/Homerwork_06.tsx
@@ -0,0 +1,34 @@
+
+import "./styles.css"
+
+interface Car {
+brand: string;
+price: number;
+isDiesel: boolean;
+}
+
+function Homerwork_06() {
+    const cars: Car[] = [
+        { brand: "BMW", price: 20000, isDiesel: true }, 
+        { brand: "Mercedes", price: 22000, isDiesel: false }, 
+        { brand: "Porshe", price: 50000, isDiesel: true }, 
+        { brand: "Nissan", price: 25000, isDiesel: false }, 
+        { brand: "Audi", price: 50000, isDiesel: true } 
+    ];
+
+    return (
+        <div className="cars_list_wrapper">
+            {cars.map((car) => (
+                <div className="card_card" key={car.brand}>
+                    <h3 className="car_title">Brand: {car.brand}</h3>
+                    <p className="car_price">Price: ${car.price}</p>
+                    <p className="car_engine">
+                        Engine Type: {car.isDiesel ? "Diesel" : "Petrol"}
+                    </p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Homerwork_06;
